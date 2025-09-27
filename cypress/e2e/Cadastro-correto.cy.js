2a265bc49622f88f67b942000c7171bfa50c37eb
FELIPE ALVES FEZZI <86987347+felipefezzi1978@users.noreply.github.com>
2025-09-27 15:45:53 -0300

    Criação de um comando Cadastro

--- cypress/e2e/Cadastro-correto.cy.js
+++ cypress/e2e/Cadastro-correto.cy.js
@@ -5,11 +5,6 @@ describe('Página de cadastro', () => {
   })
 
     it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {      
-      cy.get('[data-test="input-name"]').type('Marjorie Anoquevem');
-      cy.get('[data-test="input-email"]').type('marj@email.com');
-      cy.get('[data-test="input-password"]').type('Senha123');
-      cy.get('[data-test="input-confirm-password"]').type('Senha123');
-      cy.get('[data-test="submit-button"]').click();
-
+      cy.cadastro('Marjorie Anoquevem', 'marj@email.com', 'Senha123', 'Senha123'); 
     })
   })
