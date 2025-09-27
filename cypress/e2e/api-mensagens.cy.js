c9a7585db077dffd4bab9182b35e73ee6a9bd5eb
FELIPE ALVES FEZZI <86987347+felipefezzi1978@users.noreply.github.com>
2025-09-27 18:29:06 -0300

    Proteção de dados sensíveis

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,11 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc1OTAwMzY3MSwiZXhwIjoxNzU5MjYyODcxfQ.MO38m-yld8p3kgguOEs74GN5VpeWq7CTf147xXsp2KA`
+    
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
-            url: 'https://adopet-api-i8qu.onrender.com/mensagem',
-            headers: { authorization }
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+            headers: Cypress.env()
             }).then((res) => {
                 expect(res.status).to.be.equal(200)
                 expect(res.body).is.not.empty
